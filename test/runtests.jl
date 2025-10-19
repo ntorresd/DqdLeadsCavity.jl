37160827c3163e99b117002f605417679e1118d5
ntorresd <ntorresd@unal.edu.co>
2025-10-19 17:39:42 +0200

    test: add tests for \Omega(), \theta() and OnsiteEnergies()

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,8 +12,15 @@ end
 end
 
 @testset "Test DqdLeads structure" begin
-    dqd_leads = DqdLeads(Dqd(1. , 1., 1), Leads(2, 2, 2.))
+    dqd = Dqd(1. , 1., 1)
+    leads = Leads(2, 2, 2.)
+    dqd_leads = DqdLeads(dqd, leads)
+
     @test dqd_leads.dqd.blockade == true
     @test dqd_leads.dqd.U == Inf
     @test dqd_leads.leads.μ_avg == 0.0
+
+    @test Ω(dqd_leads.dqd) == Ω(dqd)
+    @test θ(dqd_leads.dqd) == θ(dqd)
+    @test OnsiteEnergies(dqd_leads.dqd) == OnsiteEnergies(dqd)
 end
