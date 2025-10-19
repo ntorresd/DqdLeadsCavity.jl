084b0c8ec6dd84ff53c39366adb3b8dac1f6c735
Documenter.jl <documenter@juliadocs.github.io>
2025-10-19 15:12:28 +0000

    build based on a09359b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DqdLeadsCavity.jl","page":"Home","title":"DqdLeadsCavity.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Julia package for modeling a double quantum dot with leads coupled to a microwave cavity.","category":"page"},{"location":"#Installation","page":"Home","title":"Installation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"add DqdLeadsCavity","category":"page"}]
+}
