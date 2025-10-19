507f1554d391463eff1534a7758e1d82bd922d16
ntorresd <ntorresd@unal.edu.co>
2025-10-19 16:32:17 +0200

    test: add structures tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using DqdLeadsCavity
 using Test
 
-@testset "DqdLeadsCavity.jl" begin
-    # Write your tests here.
+@testset "Test Dqd structure" begin
+    @test Dqd(1., 1, 1., 1).blockade == false;
+    @test Dqd(1, 1., 1).U == Inf;
+    @test Dqd(1, 1, 1.).blockade == true;
+end
+
+@testset "Test Leads structure" begin
+    @test Leads(1, 1., 1.).μ_avg == 0.0
+end
+
+@testset "Test DqdLeads structure" begin
+    dqd_leads = DqdLeads(Dqd(1. , 1., 1), Leads(2, 2, 2.))
+    @test dqd_leads.dqd.blockade == true
+    @test dqd_leads.dqd.U == Inf
+    @test dqd_leads.leads.μ_avg == 0.0
 end
