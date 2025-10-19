a09359b2b22f82e0dbf376923daa6bf06f06a9bb
ntorresd <ntorresd@unal.edu.co>
2025-10-19 17:03:11 +0200

    doc: add minimal website

---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+import Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(Pkg.PackageSpec(path = joinpath(@__DIR__, "..")))
+Pkg.instantiate()
+
+using Documenter
+using DqdLeadsCavity
+
+makedocs(
+    sitename = "DqdLeadsCavity.jl",
+    modules  = [DqdLeadsCavity],
+    format   = Documenter.HTML(; prettyurls = get(ENV, "CI", "false") == "true"),
+    pages    = [
+        "Home" => "index.md",
+    ],
+    checkdocs = :exports,
+)
+
+deploydocs(
+    repo      = "github.com/ntorresd/DqdLeadsCavity.jl",
+    devbranch = "main"
+)
---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+# DqdLeadsCavity.jl
+
+Julia package for modeling a double quantum dot with leads coupled to a microwave cavity.
+
+## Installation
+```julia
+add DqdLeadsCavity
+```
