aa77938f06a15008f889d5cc9a13a19e87081ba8
ntorresd <ntorresd@unal.edu.co>
2025-10-20 14:58:59 +0200

    add additional structure consistency tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,35 @@
 using DqdLeadsCavity
 using Test
 
-@testset "Test Dqd structure" begin
+@testset "Test structures' defaults" begin
+    # Dqd
     @test Dqd(1., 1, 1., 1).blockade == false;
     @test Dqd(1, 1., 1).U == Inf;
     @test Dqd(1, 1, 1.).blockade == true;
-end
 
-@testset "Test Leads structure" begin
-    @test Leads(1, 1., 1.).μ_avg == 0.0
-end
+    # Leads
+    @test Leads(1, 1., 1.).μ_avg == 0.0;
 
-@testset "Test DqdLeads structure" begin
+    # DqdLeads
     dqd = Dqd(1. , 1., 1)
     leads = Leads(2, 2, 2.)
-    dqd_leads = DqdLeads(dqd, leads)
+    dqd_leads = DqdLeads(dqd, leads, 1.0, 1.0)
 
     @test dqd_leads.dqd.blockade == true
     @test dqd_leads.dqd.U == Inf
     @test dqd_leads.leads.μ_avg == 0.0
 
-    @test Ω(dqd_leads.dqd) == Ω(dqd)
-    @test θ(dqd_leads.dqd) == θ(dqd)
-    @test OnsiteEnergies(dqd_leads.dqd) == OnsiteEnergies(dqd)
+    @test get_Ω(dqd_leads.dqd) == get_Ω(dqd)
+    @test get_θ(dqd_leads.dqd) == get_θ(dqd)
+end
+
+@testset "Test parameters cross-structure consistency" begin
+    dqd = Dqd(1. , 1., 1.)
+    leads = Leads(2., 2., 2.)
+    dqd_leads = DqdLeads(dqd, leads, 3.0, 3.0)
+    cavity = Cavity(0., 0.)
+
+    @test get_Ω(dqd_leads.dqd) == get_Ω(dqd)
+    @test get_θ(dqd_leads.dqd) == get_θ(dqd)
+    @test get_Δd(dqd, cavity) == get_Ω(dqd)
 end
