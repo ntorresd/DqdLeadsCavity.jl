949b2329f23376b925503990f72701fa89b3f128
ntorresd <ntorresd@unal.edu.co>
2026-01-07 15:36:25 -0500

    update structures

--- src/structs/dqd_leads.jl
+++ src/structs/dqd_leads.jl
@@ -1,4 +1,5 @@
 export DqdLeads
+export get_transition_rates_ge, get_fermi_ge
 
 mutable struct DqdLeads
     dqd::Dqd
@@ -19,7 +20,7 @@ function Base.show(io::IO, dqd_leads::DqdLeads)
         "Δμ = $(dqd_leads.leads.Δμ)\n",
         "μ_avg = $(dqd_leads.leads.μ_avg)\n",
         "ΓL = $(dqd_leads.ΓL)\n",
-        "ΓR = $(dqd_leads.ΓL)\n"
+        "ΓR = $(dqd_leads.ΓR)\n"
     )
 end
 
@@ -29,4 +30,25 @@ end
 
 function get_chemical_potentials(dqd_leads::DqdLeads)
     return get_chemical_potentials(dqd_leads.leads)
+end
+
+function get_transition_rates_ge(dqd_leads::DqdLeads)
+    θ = get_θ(dqd_leads.dqd)
+    ΓL = dqd_leads.ΓL
+    ΓR = dqd_leads.ΓR
+
+    sθ2 = sin(θ/2)^2
+    cθ2 = cos(θ/2)^2
+
+    # ΓLg, ΓLe, ΓRg, ΓRe (eq (89) [Potts et. al. 2021])
+    return ΓL * sθ2, ΓL * cθ2, ΓR * sθ2, ΓR * sθ2 
+end
+
+function get_fermi_ge(dqd_leads::DqdLeads)
+    ϵg, ϵe = get_eigen_energies(dqd_leads.dqd)
+    μL, μR = get_chemical_potentials(dqd_leads.leads)
+    TL, TR = dqd_leads.leads.TL, dqd_leads.leads.TR
+
+    # fLg, fLe, fRg, fRe
+    return fermi(ϵg, μL, TL), fermi(ϵe, μL, TL), fermi(ϵg, μR, TR), fermi(ϵe, μR, TR)
 end
