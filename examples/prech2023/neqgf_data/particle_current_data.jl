7821fa69e72a7d8b8bdbb83745cbb60354e3d91c
ntorresd <ntorresd@unal.edu.co>
2026-01-27 11:26:40 -0500

    fix: add option to specify NEQGF integration limits

--- examples/prech2023/neqgf_data/particle_current_data.jl
+++ examples/prech2023/neqgf_data/particle_current_data.jl
@@ -1,8 +1,9 @@
 begin
+    local tc_max = maximum(tc_range)
     dqdObj = deepcopy(dqd_leads)
     results = map(tc_range) do tc
         dqdObj.dqd.tc = tc
-        get_particle_current_neqgf(dqdObj)   # (J_L, J_R)
+        get_particle_current_neqgf(dqdObj; int_lims = (-10. * tc_max, 10. * tc_max))   # (I_L, I_R)
     end
 
     I_L_neqgf = first.(results)
