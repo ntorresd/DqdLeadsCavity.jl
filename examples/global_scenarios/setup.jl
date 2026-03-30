88d09b6097027caabe0dc89df91b6886dca806f4
ntorresd <ntorresd@unal.edu.co>
2026-03-30 21:37:30 +0200

    feat(eg): add `global_scenarios` example for fixed voltage

---
+++ examples/global_scenarios/setup.jl
@@ -0,0 +1,40 @@
+using Revise, DqdLeadsCavity
+using QuantumToolbox
+using CairoMakie, LaTeXStrings
+using ColorSchemes
+
+run_plots = true
+save_fig = false
+
+# Setup
+begin
+	# DQD parameters
+	local Δϵ = 0.0
+	local ϵ_avg = 0.0
+	local tc = 6.78
+	# local γm = 0.5 		# Relaxation rate
+	# local γϕ = 3.92 	# Dephasing rates
+	local γm = 0.0 		# Relaxation rate
+	local γϕ = 0.0 	# Dephasing rates
+
+	# dqd = Dqd(Δϵ, ϵ_avg, tc, γm, γϕ)
+	dqd = Dqd(0.0, ϵ_avg, tc, γm, γϕ, 0.0)
+
+	# Leads
+	# local T = 1e-2 # don't erase
+	local T = 10.
+	local TL = T
+	local TR = T
+	local eV = 100.
+	local μ_avg = 0.0
+
+    # DQD-Leads
+	local Γ = 1.
+	local ΓL = Γ
+	local ΓR = Γ
+	# local ΓL = 2.
+	# local ΓR = 1.
+
+	leads = Leads(TL, TR, eV, μ_avg)
+	dqd_leads = DqdLeads(dqd, leads, ΓL, ΓR)
+end
