53fbf37cd8204f2e4b74485199b2934bcd531986
ntorresd <ntorresd@unal.edu.co>
2026-03-30 21:28:53 +0200

    feat: add DQD heat current and coherence methods for NEQGFs approach

--- src/neqgf_approach.jl
+++ src/neqgf_approach.jl
@@ -1,5 +1,6 @@
 export get_particle_current_neqgf
-export get_current_heat_neqgf
+export get_heat_current_neqgf
+export get_coherence_LR_neqgf
 
 """
 Transmission function of the DQD under the wideband approximation
@@ -36,7 +37,7 @@ function _integrand_particle_current_avg_neqgf(ω, dqd_leads::DqdLeads; left::Bo
 end
 
 @doc raw"""
-Compute steady-state particle current through the non-interacting DQD using NEQGFs
+Steady-state particle current through the non-interacting DQD using NEQGFs
 according to [eq. (14) Prech et. al. 2023]
 
 # Returns
@@ -74,13 +75,13 @@ function _integrand_heat_current_neqgf(ω::Real, μ::Real, dqd_leads::DqdLeads; 
 end
 
 @doc raw"""
-Compute steady-state heat current through the non-interacting DQD using NEQGFs
+Steady-state heat current through the non-interacting DQD using NEQGFs
 according to eq. (B.3) [Potts et. al. 2021])
 
 # Returns
 - `I_avg::Real`
 """
-function get_current_heat_neqgf(
+function get_heat_current_neqgf(
     dqd_leads::DqdLeads;
     left::Bool = true,
     atol::Float64 = 1e-8,
@@ -97,4 +98,41 @@ function get_current_heat_neqgf(
 
     J, err = quadgk(integrand, ωmin, ωmax; atol = atol)
     return J
+end
+
+function _integrand_coherence_LR_neqgf(ω, dqd_leads::DqdLeads)
+    # parameters
+    tc = dqd_leads.dqd.tc
+    ϵL, ϵR = get_onsite_energies(dqd_leads.dqd)
+    μL, μR = get_chemical_potentials(dqd_leads)
+    TL, TR = dqd_leads.leads.TL, dqd_leads.leads.TR
+    ΓL, ΓR = dqd_leads.ΓL, dqd_leads.ΓR
+    # derived parameters
+    fL, fR = fermi(ϵL, μL, TL), fermi(ϵR, μR, TR)
+    # lesser Green's function
+    numerator = im * tc * ((ω - ϵR + im * ΓR / 2.) * ΓL * fL + (ω - ϵL - im * ΓL / 2.) * ΓR * fR)
+    denominator = abs((ω - ϵL + im * ΓL / 2.) * (ω - ϵR + im * ΓR / 2.) - tc^2)^2
+    G_lesser_LR = numerator / denominator
+
+    return G_lesser_LR / (2. * pi)
+end
+
+@doc raw"""
+Steady-state coherence between the left and right dot using NEQGFs
+according to eq. (A.43) [Prech2023])
+"""
+function get_coherence_LR_neqgf(
+    dqd_leads::DqdLeads;
+    atol::Float64 = 1e-8,
+    int_lims::Tuple{<:Real,<:Real}
+)
+    integrand(ω) = _integrand_coherence_LR_neqgf(ω, dqd_leads)
+
+    ωmin, ωmax = int_lims
+    if ((ωmax > 0.) && (ωmin == -ωmax)) == false
+        error("Integration limits must be symmetric around zero, i.e., (−ωmax, ωmax)")
+    end
+
+    α, err = quadgk(integrand, ωmin, ωmax; atol = atol)
+    return abs(α)
 end
